91553dc8f182b862343971afc0552d63ba29c890
ifahimkhan <khan.fahim95@hotmail.com>
2024-09-30 21:41:15 +0530

    if applied this commit will add practical no 8 javascript form validation;

--- app.js
+++
@@ -1,8 +0,0 @@
-let http = require("http");
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('This is the example of node.js web-based application \n');
-
-}).listen(5000,
-    () => console.log('Server running at http://127.0.0.1:5000/'));
