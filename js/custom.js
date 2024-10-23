a1255c9f3f30054668b3fdfa7b39b40ac8a230ff
ifahimkhan <khan.fahim95@hotmail.com>
2024-10-23 19:00:21 +0530

    if applied this commit will add angular js

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,36 @@
-var module = angular.module("customModule",[])
+var module = angular.module("appModule",[])
 
 module.controller("mController",function($scope){
-    alert("Hello World!")
+   $scope.name= "JAY"
+   $scope.rollno= "C012"
+
+
+
 });
+
+module.run(function($rootScope){
+    $rootScope.collegeName = "MukeshPatel"
+    $rootScope.fees = function(){
+        alert("fees is 500000")
+    }
+    $rootScope.color="red"
+
+    $rootScope.setColor = function(){
+
+    }
+
+})
+
+module.controller("JsonController",function($scope){
+   $scope.branches = ["Mumbai","Navi Mumbai","Jaipur"]
+
+  $scope.jsonBranch = [
+  {"branchId":"001","branchName":"Mumbai"},
+  {"branchId":"002","branchName":"Navi Mumbai"},
+  {"branchId":"003","branchName":"Jaipur"}
+  ]
+}).directive("menu", function () {
+             return {
+               template: "<select><option ng-repeat='item in branches'>{{item}}</option></select>"
+             };
+           });
